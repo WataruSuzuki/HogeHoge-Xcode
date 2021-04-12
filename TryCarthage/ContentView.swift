45b23905bd686afd6d2b6f41da8453b3ccb16c49
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-04-12 22:10:00 +0900

    Created xcode project

---
+++ TryCarthage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryCarthage
+//
+//  Created by watsuzuk on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
